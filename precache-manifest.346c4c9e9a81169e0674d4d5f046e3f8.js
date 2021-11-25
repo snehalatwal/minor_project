5869659404a3d384fb1c147e622ea10feb56021f
snehalatwal <snehalatwal68@gmail.com>
2021-11-25 22:22:18 +0530

    Updates

---
+++ precache-manifest.346c4c9e9a81169e0674d4d5f046e3f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b70819d758bb37cc32b9e2e3dd6b6bf",
+    "url": "/minor_project/index.html"
+  },
+  {
+    "revision": "68294e4cbc1ceec56d66",
+    "url": "/minor_project/static/css/main.2ca93671.chunk.css"
+  },
+  {
+    "revision": "91f6beb32aeb6d3b6e48",
+    "url": "/minor_project/static/js/2.e5c81922.chunk.js"
+  },
+  {
+    "revision": "4e9e6a426e4934b119d62e02d6fa5a5c",
+    "url": "/minor_project/static/js/2.e5c81922.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68294e4cbc1ceec56d66",
+    "url": "/minor_project/static/js/main.e3411e83.chunk.js"
+  },
+  {
+    "revision": "a400338f5321b6ace053",
+    "url": "/minor_project/static/js/runtime-main.544eb713.js"
+  }
+]);
